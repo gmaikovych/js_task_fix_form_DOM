f05b712263d272bb7601a273c29cd401b77e4a3f
Galyna Maikovych <gmaik.com@gmail.com>
2025-04-03 23:11:04 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (let i = 0; i < inputs.length; i++) {
+  const labelHTML = `<label class= "field-label" for="${inputs[i].id}">${inputs[i].name}</label>`;
+
+  inputs[i].insertAdjacentHTML('beforebegin', labelHTML);
+
+  inputs[i].placeholder = inputs[i].name.toUpperCase();
+}
